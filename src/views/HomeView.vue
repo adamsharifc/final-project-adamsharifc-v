9ab05cb4fdf4722d01ef00d50f6095cb13d8c028
adamsharifc <97190119+adamsharifc@users.noreply.github.com>
2023-11-09 21:05:10 -0500

    displayed bugs on main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <script setup>
-import {RouterLink, onMounted} from 'vue-router';
-import {ref} from 'vue';
+import {RouterLink} from 'vue-router';
+import {ref, onMounted} from 'vue';
 import axios from 'axios';
 
 const data = ref([]);
 
 const getData = async () => {
     try {
-        const response = await axios.get('https://final-project-adamsharifc-p.vercel.app/api/bugReports');
+        const response = await axios.get('https://final-project-adamsharifc-p.vercel.app/api/getBugReports');
         data.value = response.data;
         console.log(JSON.stringify(data.value));
     } catch (error) {
@@ -20,6 +20,12 @@ onMounted(getData);
 </script>
 <template>
 <h1>SQUASHEM</h1>
+
+<div v-for="(item, index) in data" :key="index">
+    <span>{{item.title}}</span>
+
+</div>
+
 
 <nav>
     <router-link to="/AddBug">Add Bug</router-link> |
