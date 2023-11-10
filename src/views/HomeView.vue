eb425ca106364051ee3843d666f9cbfbeea540d7
adamsharifc <97190119+adamsharifc@users.noreply.github.com>
2023-11-09 19:44:15 -0500

    called getBugReports on Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
 <script setup>
-import {RouterLink} from 'vue-router'
+import {RouterLink, onMounted} from 'vue-router';
+import {ref} from 'vue';
+import axios from 'axios';
+
+const data = ref([]);
+
+const getData = async () => {
+    try {
+        const response = await axios.get('https://final-project-adamsharifc-p.vercel.app/api/bugReports');
+        data.value = response.data;
+        console.log(JSON.stringify(data.value));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+onMounted(getData);
+
 </script>
 <template>
 <h1>SQUASHEM</h1>
